d8a9b96ab931948b53b5f2bad2d0a961562bc47d
YodgorGA <yodgorgaffarov@gmail.com>
2023-10-11 12:09:54 +0400

    Make the Taro/Result page

--- src/app/(pages)/taro/result/page.tsx
+++ src/app/(pages)/taro/result/page.tsx
@@ -1,5 +1,6 @@
 import {FC} from 'react';
 import S from './result.module.scss';
+import { Title } from '@/components/shared';
 
 interface ResultProps {
     
@@ -10,8 +11,12 @@ export const Result:FC<ResultProps> = ({...ResultProps}) =>{
     return ( 
         <section className={S.container}>
             <div className={S.wrapper}>
-                <div className={S.title}></div>
+                <Title children='Результат'/>
+                <div className={S.result_container}>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque accusamus perferendis nisi at, distinctio hic! Alias ad doloremque quasi illum cum rem dolorem quod, magni odio facere maxime quaerat sit.</p>
+                </div>
             </div>
+
         </section>
     )
 }
