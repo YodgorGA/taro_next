972a68b8fc2a0afe3c6a5824b9d455e5452bbf4d
YodgorGA <yodgorgaffarov@gmail.com>
2023-10-19 00:20:08 +0400

    replace html to TaroCards components

--- src/app/(pages)/taro/result/page.tsx
+++ src/app/(pages)/taro/result/page.tsx
@@ -1,6 +1,7 @@
 import {FC} from 'react';
 import S from './result.module.scss';
 import { Cross, Title } from '@/components/shared';
+import { TaroCards } from '@/components/widgets';
 
 interface ResultProps {
     
@@ -14,23 +15,7 @@ export const Result:FC<ResultProps> = ({...ResultProps}) =>{
             <div className={S.wrapper}>
                 <Title children='Результат'/>
                 <div className={S.result_container}>
-                <div className={S.cards_container}>
-                    <div className={S.cards_cardItemContainer}>
-                        <div key={Math.random()} className={S.cards_cardItem}></div>
-                    </div>
-                    <div className={S.cards_cardItemContainer}>
-                        <div key={Math.random()} className={S.cards_cardItem}></div>
-                    </div>
-                    <div className={S.cards_cardItemContainer}>
-                        <div key={Math.random()} className={S.cards_cardItem}></div>
-                    </div>
-                    <div className={S.cards_cardItemContainer}>
-                        <div key={Math.random()} className={S.cards_cardItem}></div>
-                    </div>
-                    <div className={S.cards_cardItemContainer}>
-                        <div key={Math.random()} className={S.cards_cardItem}></div>
-                    </div>
-                </div>
+                    <TaroCards/>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque accusamus perferendis nisi at, distinctio hic! Alias ad doloremque quasi illum cum rem dolorem quod, magni odio facere maxime quaerat sit.</p>
                 </div>
             </div>
