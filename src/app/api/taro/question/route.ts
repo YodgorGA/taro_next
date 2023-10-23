d2b3a05510ff474482896c3a5765d35220978043
YodgorGA <yodgorgaffarov@gmail.com>
2023-10-23 17:52:02 +0400

    small changes

--- src/app/api/taro/question/route.ts
+++ src/app/api/taro/question/route.ts
@@ -1,15 +1,35 @@
 import { NextResponse } from "next/server";
 
 
-// const chatReq = async (message:string) =>{
-//     const resp = await fetch('https://api.openai.com/v1/chat/completions',{
-//         method:'POST',
-//         body:{}
-//     })
-// }
+const chatReq = async (body:{
+    model:string,
+    messages:[
+        {
+            role:string,
+            content:string
+        }
+    ]
+}) =>{
+    const resp = await fetch('https://api.openai.com/v1/chat/completions',{
+        method:'POST',
+        headers:{
+            "Content-Type":"aplication/json",
+            "Authorization": `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`
+        },
+        body:JSON.stringify(body)
+    })
+
+    return resp
+}
 
 export async function POST(request:Request,){
     const body = await request.json()
-
-    return NextResponse.json({body});
+    const chatResp = await chatReq({
+        model:'gpt-3.5-turbo',
+        messages:[{
+            role:`user`,
+            content: body
+        }]
+    })
+    return NextResponse.json({chatResp});
 }
