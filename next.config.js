818fbcff264f36106ac4d04716738a5660bb02bf
YodgorGA <yodgorgaffarov@gmail.com>
2023-11-06 02:22:45 +0400

    add assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-// const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
     images:{
         deviceSizes:[1920,1152,1151,640]
     },
     reactStrictMode:false,
-    // assetPrefix: isProd ? 'https://cdn.portalofmagic.ru' : undefined,
+    assetPrefix: isProd ? 'https://cdn.portalofmagic.ru' : undefined,
 }
 
 module.exports = nextConfig
