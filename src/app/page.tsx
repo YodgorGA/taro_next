26e435e71b5684d74ad78364e6b9ac2e18082eb2
YodgorGA <yodgorgaffarov@gmail.com>
2023-11-29 22:30:07 +0400

    Make Main looks great for desc and mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import {FC} from 'react';
 import S from './main.module.scss';
 import Image from 'next/image';
 import SandClock from '/public/assets/images/sandClocks.png'
-import TelegramIcon from '/public/assets/icons/telegram 1.png'
-import EmaiIcon from '/public/assets/icons/email 1.png'
-import Logo from '/public/assets/icons/magic_portal.png'
 import { Header } from '@/components/widgets';
-import { Footer, StoreUpdater } from '@/components/shared';
+import { Footer } from '@/components/shared';
 import Darkball from '/public/assets/images/dbl.png';
 import Lightball from '/public/assets/images/lbl.png';
-import Link from 'next/link';
+import TaroCardIcon from '/public/assets/icons/taro.png';
+import DreamcatcherIcon from '/public/assets/icons/dreamcatcher.png';
+import NubmerologyIcon from '/public/assets/icons/numerology.png';
 
 interface MainPageProps {
     
@@ -30,29 +29,43 @@ const MainPage:FC<MainPageProps> = ({...MainPageProps}) =>{
             </div>
             <Header/>
             <div className={S.pageContent}>
-                <div className={S.hero_container}>
-                    <div className={S.logo_container}>
-                        <Image alt="Портал магии" src={Logo} fill/>
-                    </div>
-                    <div className={S.contactUs_container}>
-                        <div className={S.contactUs__tg}>
-                            <Image fill src={TelegramIcon} alt="Contact us on Telegram" />
-                        </div>
-                        <div className={S.contactUs__email}> 
-                            <Image fill src={EmaiIcon} alt="Contact us on mail" />
-                        </div>
-                        
-                       
-                    </div>
-                    <div className={S.description_container}>
-                        <p>Таро, числа, сны. Откройте двери к своему волшебному будущему</p>
-                    </div>
+                <div className={S.welcomeText}>
+                    <p>Добро пожаловать на Портал магии</p>
                 </div>
                 <div className={S.image_container}>
-                    <div className={S.divider}></div>
                     <Image priority src={SandClock} alt="Портал магии" className={S.sandclockImage}/>
-                    <div className={S.brandname_container}>
-                        <p>Портал магии</p>
+                </div>
+                <div className={S.aboutText}>
+                    <div className={S.aboutText_heroText}>
+                        <p>Давайте вместе исследовать тайны и магию мира Таро, нумерологии и снов. Портал магии - ваш источник для понимания и самопознания через эти увлекательные искусства.</p>
+                    </div>
+                </div>
+                <div className={S.offers}>
+                    <div className={S.offers_header}>
+                        <p>Что мы предлагаем?</p>
+                    </div>
+                    <div className={S.offers_list}>
+                        <li className={S.offers_listItem}><div className={S.listItemImg_container}><Image fill src={TaroCardIcon} alt={"https://www.flaticon.com/ru/free-icons/"}/></div><p><span style={{color:'#E5C59E'}}>Таро:</span> Разгадайте символику карт Таро, предсказывайте будущее и познайте себя с помощью мудрости старинных карт</p></li>
+                        <li className={S.offers_listItem}><div className={S.listItemImg_container}><Image fill src={NubmerologyIcon } alt={"https://www.flaticon.com/ru/free-icons/"}/></div><p><span style={{color:'#E5C59E'}}>Нумерология:</span> Понимайте, как числа влияют на вашу жизнь и открывают вам путь к самопознанию</p></li>
+                        <li className={S.offers_listItem}><div className={S.listItemImg_container}><Image fill src={DreamcatcherIcon} alt={"https://www.flaticon.com/ru/free-icons/"}/></div><p><span style={{color:'#E5C59E'}}>Сонник:</span> Интерпретируйте ваши сновидения, раскрывайте их смысл и получайте ценные инсайты.</p></li>
+                    </div>
+                </div>
+                <div className={S.purposes}>
+                    <div className={S.purposes_header}>
+                        <p>Почему выбрать Портал магии?</p>
+                    </div>
+                    <div className={S.purposes_list}>
+                        <li><span style={{color:'#E5C59E'}}>Глубокие знания:</span> Мы являемся экспертами в области Таро, нумерологии и снов, и предлагаем качественные услуги и анализ</li>
+                        <li><span style={{color:'#E5C59E'}}>Точность:</span> Наши алгоритмы обеспечивают точные и надежные результаты в интерпретации Таро, чисел и снов</li>
+                        <li><span style={{color:'#E5C59E'}}>Конфиденциальность:</span> Ваша конфиденциальность у нас на первом месте, и мы гарантируем полную безопасность ваших данных</li>
+                        <li><span style={{color:'#E5C59E'}}>Индивидуальный подход:</span> Мы адаптируем наши услуги к вашим уникальным потребностям</li>
+                    </div>
+                    <div className={S.afterall}>
+                        <div className={S.afterall_description}>
+                            <p>
+                                Если вы готовы начать путь самопознания с использованием современных технологий, свяжитесь с нами прямо сейчас. Наши алгоритмы готовы помочь вам исследовать мир Таро, нумерологии и снов, и разгадывать тайны вашей жизни.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 {/* <div className={S.buttons_container}>
